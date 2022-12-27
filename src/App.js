70b29c239d928fa207dec449520c53288364635f
adil-khursheed <adilkhursheed60@gmail.com>
2022-12-27 18:02:15 +0530

    Initial commit create react app

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import { useEffect } from 'react';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=4ca25813';
+
+const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className='search'>
+        <input
+          placeholder='Search for movies'
+          value='Superman'
+          onChange={()=>{}}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => {}}
+        />
+      </div>
+
+      <div className='container'>
+
+        {/* TODO */}
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
